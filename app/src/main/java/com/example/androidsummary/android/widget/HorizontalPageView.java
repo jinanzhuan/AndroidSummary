bb0a0e478e4aa70017648f40d218be5dae86378c
neil <neil.liu@easemob.com>
2020-07-23 18:43:31 +0800

    add custom horizontal page view.

--- app/src/main/java/com/example/androidsummary/android/widget/HorizontalPageView.java
+++ app/src/main/java/com/example/androidsummary/android/widget/HorizontalPageView.java
@@ -1,5 +1,8 @@
 package com.example.androidsummary.android.widget;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Rect;
 import android.util.AttributeSet;
@@ -8,7 +11,9 @@ import android.util.SparseArray;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Scroller;
 
 import androidx.annotation.NonNull;
@@ -17,12 +22,16 @@ import androidx.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 基本能够实现左右滑动，及适应子控件大小的问题
+ * 需要解决的问题：1、惯性滑动。2、计算边界问题
+ */
 public class HorizontalPageView extends ViewGroup {
-    private int numRows = 2;
+    private int numRows = 3;
     private int numColumns = 2;
     private int onePageCount;
-    private int horizontalSpacing;
-    private int verticalSpacing;
+    private int horizontalSpacing = 0;
+    private int verticalSpacing = 0;
     private SparseArray<Rect> allItemFrames = new SparseArray<>();
     private GestureDetector gestureDetector;
     private Scroller scroller;
@@ -31,6 +40,15 @@ public class HorizontalPageView extends ViewGroup {
     private int scrollX;
     private int startX;
     private int startY;
+    private int itemWidth;
+    private int itemHeight;
+
+    private int mMinFlingVelocity;
+    private int mMaxFlingVelocity;
+    private boolean isFirst;
+    private int mWidth;
+    private int mHeight;
+    private int mLastFlingX;
 
     public HorizontalPageView(@NonNull Context context) {
         this(context, null);
@@ -44,20 +62,19 @@ public class HorizontalPageView extends ViewGroup {
         super(context, attrs, defStyleAttr);
         onePageCount = numRows * numColumns;
 
-        gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
-            @Override
-            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-                scrollBy((int) distanceX, 0);
-                return super.onScroll(e1, e2, distanceX, distanceY);
-            }
+        ViewConfiguration vc = ViewConfiguration.get(context);
+        mMinFlingVelocity = vc.getScaledMinimumFlingVelocity();
+        mMaxFlingVelocity = vc.getScaledMaximumFlingVelocity();
 
-            @Override
-            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-                return super.onFling(e1, e2, velocityX, velocityY);
-            }
-        });
-
+        gestureDetector = new GestureDetector(context, new MySimpleOnGestureListener());
         scroller = new Scroller(context);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w;
+        mHeight = h;
     }
 
     @Override
@@ -68,35 +85,50 @@ public class HorizontalPageView extends ViewGroup {
         int widthMode = MeasureSpec.getMode(widthMeasureSpec);
         int heightMode = MeasureSpec.getMode(heightMeasureSpec);
 
-        int itemUsedWidth = getItemUsableWidth(width);
-        int itemWidthMeasureSpec = MeasureSpec.makeMeasureSpec(itemUsedWidth, MeasureSpec.EXACTLY);
+        int itemUsableWidth = getItemUsableWidth(width);
+        int itemWidthMeasureSpec = MeasureSpec.makeMeasureSpec(itemUsableWidth, MeasureSpec.EXACTLY);
 
         int totalHeight = 0;//总的高度
-        int totalWidth = 0;//总的高度
 
         List<View> childViews = getVisibleChildViews();
-        int pages = getPageSize();
 
         //这里只需要计算出单页的高度即可，默认认为item的宽高是一致的
-        int childHeight = 0;
-        if(childViews.size() > 0) {
-            View child = childViews.get(0);
+        int maxChildHeight = 0;
+        int maxChildWidth = 0;
+        for(int i = 0; i < childViews.size(); i++) {
+            View child = childViews.get(i);
             measureChild(child, itemWidthMeasureSpec, heightMeasureSpec);
-            Log.e("TAG", "measureChild getHeight = "+child.getMeasuredHeight());
-            childHeight = itemUsedWidth * child.getMeasuredHeight() / child.getMeasuredWidth();
-            Log.e("TAG", "measureChild childHeight = "+childHeight);
-            int itemHeightMeasureSpec = MeasureSpec.makeMeasureSpec(childHeight, MeasureSpec.EXACTLY);
-            measureChild(child, itemWidthMeasureSpec, itemHeightMeasureSpec);
-            Log.e("TAG", "measureChild getHeight22 = "+child.getMeasuredHeight());
+            int childHeight = child.getMeasuredHeight();
+            maxChildHeight = Math.max(childHeight, maxChildHeight);
+            maxChildWidth = Math.max(child.getMeasuredWidth(), maxChildWidth);
         }
+
+        //如果条目的宽度超过可用的宽度，则需要对宽高进行同比例缩放
+        if(maxChildWidth > itemUsableWidth) {
+            maxChildHeight = itemUsableWidth * maxChildHeight / maxChildWidth;
+            maxChildWidth = itemUsableWidth;
+        }
+
+        int rows;//所需行数
         if(childViews.size() >= onePageCount) {
             //计算子控件的单个高度*numRows即可
-            totalHeight = childHeight * numRows;
+            rows = numRows;
         }else {
-            int rows = (int) Math.ceil(childViews.size() * 1.0f / numColumns);
-            totalHeight = childHeight * rows;
+            rows = (int) Math.ceil(childViews.size() * 1.0f / numColumns);
         }
-        totalWidth = itemUsedWidth * pages;
+        totalHeight = maxChildHeight * rows + verticalSpacing * (rows - 1);
+
+        //如果条目的高度超过可用的高度，则需对宽度进行缩放
+        if(totalHeight > height - getPaddingTop() - getPaddingBottom()) {
+            int itemScaleHeight = (height - getPaddingTop() - getPaddingBottom() - verticalSpacing * (rows - 1))/ rows;
+            int itemScaleWidth = maxChildWidth * itemScaleHeight / maxChildHeight;
+            maxChildHeight = itemScaleHeight;
+            maxChildWidth = itemScaleWidth;
+        }
+
+        //最后得到条目的宽和高
+        itemHeight = maxChildHeight;
+        itemWidth = maxChildWidth;
 
         if(heightMode == MeasureSpec.EXACTLY) {
             totalHeight = height;
@@ -106,7 +138,7 @@ public class HorizontalPageView extends ViewGroup {
         }else {
             totalHeight = totalHeight + getPaddingTop() + getPaddingBottom();
         }
-        setMeasuredDimension(totalWidth, totalHeight);
+        setMeasuredDimension(width, totalHeight);
     }
 
     @Override
@@ -121,11 +153,16 @@ public class HorizontalPageView extends ViewGroup {
             for(int ro = 0; ro < numRows; ro++) {
                 for(int co = 0; co < numColumns; co++) {
                     int index = p * onePageCount + ro * numColumns + co;
-                    Log.e("TAG", "index = "+index);
                     if(index == getVisibleChildViews().size()) {
+                        //跳出多重循环
+                        co = numColumns;
+                        ro = numRows;
+                        p = onePageCount;
                         break;
                     }
-                    View child = childViews.get(p);
+                    View child = childViews.get(index);
+                    //根据分配条目的宽高，再重新测量一次
+                    child.measure(MeasureSpec.makeMeasureSpec(itemWidth, MeasureSpec.EXACTLY), MeasureSpec.makeMeasureSpec(itemHeight, MeasureSpec.EXACTLY));
                     LayoutParams layoutParams = child.getLayoutParams();
                     int leftMargin =  0;
                     int rightMargin = 0;
@@ -141,6 +178,8 @@ public class HorizontalPageView extends ViewGroup {
                     int childWidth = child.getMeasuredWidth() + leftMargin + rightMargin;
                     int childHeight = child.getMeasuredHeight() + topMargin + bottomMargin;
 
+                    int itemUsableWidth = getItemUsableWidth(getWidth());
+
                     //记录显示范围
                     Rect rect = allItemFrames.get(index);
                     if (rect == null) {
@@ -148,13 +187,14 @@ public class HorizontalPageView extends ViewGroup {
                     }
                     int hSpacing = 0;
                     int vSpacing = 0;
-                    if(co != 0 && co != numColumns - 1) {
+                    if(co != 0) {
                         hSpacing = co * horizontalSpacing;
                     }
-                    if(ro != 0 && ro != numRows - 1) {
+                    if(ro != 0) {
                         vSpacing = ro * verticalSpacing;
                     }
-                    int x = paddingLeft + p * getWidth() + co * childWidth + hSpacing;
+                    //p * getWidth():x轴方向上第几页
+                    int x = paddingLeft + p * getWidth() + co * itemUsableWidth + hSpacing + itemUsableWidth / 2 - childWidth / 2;
                     int y = paddingTop + ro * childHeight + vSpacing;
                     rect.set(x, y, childWidth + x, childHeight + y);
                     allItemFrames.put(index, rect);
@@ -173,16 +213,25 @@ public class HorizontalPageView extends ViewGroup {
         }
     }
 
+    float preX, curX;
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         gestureDetector.onTouchEvent(event);
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN :
-
+                preX = event.getX();
+                isFirst = true;
                 break;
             case MotionEvent.ACTION_MOVE :
+                curX = event.getX();
                 scrollX = getScrollX();
-                position = (getScrollX() + getWidth() / 2) / getWidth();
+                //从右往左滑动
+                if(preX > curX) {
+                    position = (scrollX + getWidth() * 4 / 5) / getWidth();
+                }else {
+                    position = (scrollX + getWidth() / 5) / getWidth();
+                }
                 if(position >= getPageSize()) {
                     position = getPageSize() - 1;
                 }
@@ -225,10 +274,31 @@ public class HorizontalPageView extends ViewGroup {
 
     @Override
     public void computeScroll() {
+        //just use for scroller
         if(scroller.computeScrollOffset()) {
             scrollTo(scroller.getCurrX(), 0);
             postInvalidate();
         }
+    }
+
+    private class MySimpleOnGestureListener extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            //dragging
+            scrollBy((int) distanceX, 0);
+            return super.onScroll(e1, e2, distanceX, distanceY);
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            fling(e2.getX(), e2.getY(), velocityX, velocityY);
+            return super.onFling(e1, e2, velocityX, velocityY);
+        }
+    }
+
+    private void fling(float startX, float startY, float velocityX, float velocityY) {
+        Log.e("TAG", "startX = "+startX + " startY = "+startY + " velocityX = "+velocityX);
+        scroller.fling((int)startX, (int)startY, -(int)velocityX, -(int)velocityY, Integer.MIN_VALUE, Integer.MAX_VALUE, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 
     /**
@@ -268,5 +338,7 @@ public class HorizontalPageView extends ViewGroup {
     private int getPageSize() {
         return (int) Math.ceil(getVisibleChildViews().size() * 1.0f / onePageCount);
     }
+
+
 }
 
